bf7e8615b4c23784455a88d8235840050ccd546b
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-02-07 21:17:19 +0100

    Added Util functions

--- Code/input_mod.py
+++ Code/input_mod.py
@@ -2,7 +2,16 @@ class InputNW:
     def __init__(self, value, weight):
         self.value = value
         self.weight = weight
-        self.calculated_value = value * weight
 
     def getValue(self):
-        return self.calculated_value
+        calculated_value = self.value * self.weight
+        return calculated_value
+
+    def setWeight(self, weight):
+        self.weight = weight
+
+    def setValue(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f'This input had a value of {self.value} and a weight of {self.weight}'
