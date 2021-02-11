31d1f2e3de25e4f018358fe0be42038508bdd924
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-02-11 09:31:49 +0100

    Added documentation

--- Code/Perceptron/perceptron_mod.py
+++ Code/Perceptron/perceptron_mod.py
@@ -1,17 +1,24 @@
 class InputNW:
-    def __init__(self, inputWeight: [float], bias: float = 0, threshold: float = 0,activationType: str = "Stepper"):
+    def __init__(self, inputWeight: [float], bias: float = 0, threshold: float = 0, activationType: str = "Stepper"):
         self.inputWeight = inputWeight
         self.bias = bias
         self.threshold = threshold
-        self.activationType = activationType
+        self.activationType = activationType  # This can be expanded on to work with the sigmoid function
 
     def activate(self, inputvaluelist: [float]):
+        """
+        This function sets the Perceptron in action. Give a list of inputs for the perceptron the the output wil be calculated
+        :param inputvaluelist: A Float list with a set of inputs equel to the amouts of weights of this perceptron
+        """
         self.inputvaluelist = inputvaluelist
         self.inputlist = list(zip(inputvaluelist, self.inputWeight))
         self.output = self.getOutput()
 
-
     def getValue(self):
+        """
+        This function returns the sum of the values given to the perceptron before going through the activation function
+        :return: sum of inputs with weights and bias
+        """
         input_sum = 0
         for inp in self.inputlist:
             input_sum += inp[0] * inp[1]
@@ -19,23 +26,18 @@ class InputNW:
         return input_sum
 
     def getOutput(self):
+        """
+        This function return the output of the perceptron going through the set activation type
+        :return:
+        """
         if self.activationType == 'Sigmoid':
-            return 0 # nog maken
-        elif self.activationType == 'Stepper': return self.getValue() >= self.threshold # Stepper activation as boolean
-        else: return self.getValue() >= self.threshold # Stepper activation as boolean
+            return 0  # nog maken
+        elif self.activationType == 'Stepper':
+            return self.getValue() >= self.threshold  # Stepper activation as boolean
+        else:
+            return self.getValue() >= self.threshold  # Stepper activation as boolean
 
     def __str__(self):
         return f'This input has {self.inputlist} as input' \
                f' and has {self.inputWeight} as input' \
                f' and input has {self.bias} as bias\n '
-
-    # Old Init code
-    # def __init__(self, inputvaluelist: [float], inputWeight: [float], bias: float = 0, threshold: float = 0,activationType: str = "Stepper"):
-    #     # Input list contain as first value the raw input and as 2nd value the weight of that value
-    #     self.inputvaluelist = inputvaluelist
-    #     self.inputWeight = inputWeight
-    #     self.inputlist = list(zip(inputvaluelist, inputWeight))
-    #     self.bias = bias
-    #     self.threshold = threshold
-    #     self.activationType = activationType
-    #     self.output = self.getOutput()
