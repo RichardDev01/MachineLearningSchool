855999f4310de30d1715f99e80d6fef852484f49
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-02-22 12:25:50 +0100

    Merge Perceptron with Neuron update code

--- Code/Neuron/neuron_mod.py
+++ Code/Neuron/neuron_mod.py
@@ -1,3 +1,5 @@
+from typing import List
+
 e = 2.71828
 
 
@@ -6,7 +8,7 @@ def sigmoid(z):
 
 
 class InputNW:
-    def __init__(self, inputWeight: [float], bias: float = 0, threshold: float = 0, activationType: str = "Stepper",
+    def __init__(self, inputWeight: List[float], bias: float = 0, threshold: float = 0, activationType: str = "Stepper",
                  idPerceptron: str = "ND"):
         self.inputWeight = inputWeight
         self.bias = bias
@@ -14,7 +16,7 @@ class InputNW:
         self.activationType = activationType  # This can be expanded on to work with the sigmoid function
         self.id = idPerceptron
 
-    def activate(self, inputvaluelist: [float]):
+    def activate(self, inputvaluelist: List[float]):
         """
         This function sets the Perceptron in action. Give a list of inputs for the perceptron the the output wil be
          lculated
@@ -50,8 +52,49 @@ class InputNW:
         else:
             return self.getValue() >= self.threshold  # Stepper activation as boolean
 
+    def update(self, trainset: ([], []), epoch:int = 1, learningRate: float = 0.1):
+
+        for runtime in range(epoch):
+            for index, example in enumerate(trainset):
+                # output y = f(w ∙ x) met activatiefunctie f en y ∈ {0, 1}
+                self.activate(example[0])
+                output = self.getOutput()
+
+                target = example[1][0]
+
+                # error e = d – y
+                error = target - output
+
+                # Δw = η ∙ e ∙ x
+                # w' = w + Δw
+                for index, weight in enumerate(self.inputWeight):
+                    self.inputWeight[index] += learningRate * error * example[0][index]
+
+                # Δb = η ∙ e
+                # b' = b + Δb
+                self.bias += learningRate * error
+
+
+    def error(self, trainset: ([], [])):
+    #MSE = Σ | d – y |^2 / n
+
+        errorSum = 0.0
+        for index, example in enumerate(trainset):
+            # | d – y |^2
+            self.activate(example[0])
+            output = self.getOutput()
+
+            target = example[1][0]
+
+            error = target - output
+            errorSum += error**2
+
+        # Σ |errorSum| / n
+        errorSum = errorSum / len(trainset)
+        return errorSum
+
     def __str__(self):
         return f'This is a {self.id} Perceptron' \
-               f' and has {self.inputlist} as input' \
+               f' and has {self.inputvaluelist} as input' \
                f' and has {self.inputWeight} as weights' \
                f' and has {self.bias} as bias\n '
